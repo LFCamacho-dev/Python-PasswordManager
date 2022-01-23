98f0ce9e7a2d62ef8b103fb73bd9f2de277a990c
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-23 16:07:35 -0600

    added a password generator module, imported pyperclip and improved the GUI

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from tkinter import *
+from tkinter import messagebox
+from generator import generate
+import pyperclip
+
 # import pandas
-import pandas
 
 BG_COLOR = "#dddddd"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+def generate_strong_password():
+    password_entry.delete(0, END)
+    strong_password = generate()
+    pyperclip.copy(strong_password)
+    generate_button.config(text="Copied ", fg="green")
+    window.after(1000, lambda: copy_to_clipboard())
+    password_entry.insert(0, strong_password)
 
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_data():
     data_entry = {
@@ -14,22 +27,37 @@ def save_data():
         "User": f"{user_entry.get()}",
         "Password": f"{password_entry.get()}"
     }
-    # data_df = pandas.DataFrame([data_entry])
 
-    with open("data_test.txt", "a") as f:
-        f.write(f"{str(data_entry)}\n")
-        # f.write(str(data_df))
+    if len(website_entry.get()) == 0 or len(user_entry.get()) == 0 or len(password_entry.get()) == 0:
+        messagebox.showinfo(title="Empty Fields", message="Some fields were left empty")
+        return
+    else:
+        is_ok = messagebox.askokcancel(title=website_entry.get(),
+                                       message=f"These are the details entered:\n\n"
+                                               f"Email: {user_entry.get()} \n"
+                                               f"Password: {password_entry.get()}\n\n"
+                                               f"Is it OK to save?")
+
+        if not is_ok:
+            return
+        elif is_ok:
+            with open("data.txt", "a") as f:
+                f.write(f"{str(data_entry)}\n")
 
     website_entry.delete(0, END)
     # user_entry.delete(0, END)
     password_entry.delete(0, END)
-    save_button.config(text="Password Stored", fg="green")
+    save_button.config(text="Password Stored ✓", fg="green")
 
     window.after(1000, lambda: save_button_behaviour())
 
 
 def save_button_behaviour():
-    save_button.config(text="Save", fg="black")
+    save_button.config(text="SAVE TO FILE ", fg="black")
+
+
+def copy_to_clipboard():
+    generate_button.config(text="Generate", fg="black")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -62,11 +90,10 @@ password_label.grid(column=1, row=4, sticky="e", pady=3)
 password_entry = Entry(font=("Arial", 10, "normal"), width=30, highlightthickness=0)
 password_entry.grid(column=2, row=4, sticky="w", pady=3)
 
-generate_button = Button(text="Generate ", highlightthickness=0, width=8)
+generate_button = Button(text=" Generate", highlightthickness=0, width=8, command=lambda: generate_strong_password())
 generate_button.grid(column=3, row=4, sticky="w", pady=3)
 
-save_button = Button(text="Save", highlightthickness=0, width=40, command=lambda: save_data())
+save_button = Button(text="SAVE TO FILE ", highlightthickness=0, width=40, command=lambda: save_data())
 save_button.grid(column=2, row=5, columnspan=2, sticky="w", pady=3)
-
 
 window.mainloop()
